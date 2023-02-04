856d6ec3b803f687264cac2c8149a5815167f37b
zhanglinjuan <zhanglinjuan20s@ict.ac.cn>
2023-02-04 14:30:03 +0800

    RequestArb: fix bug in ren of RefillBuf and ReleaseBuf

--- src/main/scala/coupledL2/RequestArb.scala
+++ src/main/scala/coupledL2/RequestArb.scala
@@ -160,18 +160,19 @@ class RequestArb(implicit p: Parameters) extends L2Module {
 
   // MSHR task
   val mshrTask_s2 = task_s2.valid && task_s2.bits.mshrTask
-  // For GrantData, read refillBuffer
-  // Caution: GrantData-alias may read DataStorage or ReleaseBuf instead
-  io.refillBufRead_s2.valid := mshrTask_s2 && !task_s2.bits.useProbeData && task_s2.bits.fromA &&
+  val mshrTask_s2_a_upwards = task_s2.bits.fromA &&
     (task_s2.bits.opcode === GrantData || task_s2.bits.opcode === Grant ||
       task_s2.bits.opcode === AccessAckData)
+  // For GrantData, read refillBuffer
+  // Caution: GrantData-alias may read DataStorage or ReleaseBuf instead
+  io.refillBufRead_s2.valid := mshrTask_s2 && !task_s2.bits.useProbeData && mshrTask_s2_a_upwards
   io.refillBufRead_s2.id := task_s2.bits.mshrId
   // For ReleaseData or ProbeAckData, read releaseBuffer
   // channel is used to differentiate GrantData and ProbeAckData
   io.releaseBufRead_s2.valid := mshrTask_s2 && (
     task_s2.bits.opcode === ReleaseData ||
     task_s2.bits.fromB && task_s2.bits.opcode === ProbeAckData ||
-    task_s2.bits.fromA && task_s2.bits.useProbeData)
+    task_s2.bits.fromA && task_s2.bits.useProbeData && mshrTask_s2_a_upwards)
   io.releaseBufRead_s2.id := task_s2.bits.mshrId
   assert(!io.refillBufRead_s2.valid || io.refillBufRead_s2.ready)
   assert(!io.releaseBufRead_s2.valid || io.releaseBufRead_s2.ready)
