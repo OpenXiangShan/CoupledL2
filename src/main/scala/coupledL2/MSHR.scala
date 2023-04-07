e7c64fddfc8824d0ab5cf74c395b684cb94c0daa
wangkaifan <wangkaifan@ict.ac.cn>
2023-04-07 09:55:10 +0800

    mshr: block replaced block until release sent

--- src/main/scala/coupledL2/MSHR.scala
+++ src/main/scala/coupledL2/MSHR.scala
@@ -393,7 +393,7 @@ class MSHR(implicit p: Parameters) extends L2Module {
   io.status.valid := status_reg.valid
   io.status.bits <> status_reg.bits
   // For A reqs, we only concern about the tag to be replaced
-  io.status.bits.tag := Mux(state.s_release, req.tag, dirResult.tag) // s_release is low-as-valid 
+  io.status.bits.tag := Mux(state.w_release_sent, req.tag, dirResult.tag) // s_release is low-as-valid
   io.status.bits.nestB := status_reg.valid && state.w_releaseack && state.w_rprobeacklast && state.w_pprobeacklast && !state.w_grantfirst
   // wait for resps, high as valid
   io.status.bits.w_c_resp := !state.w_rprobeacklast || !state.w_pprobeacklast || !state.w_pprobeack
