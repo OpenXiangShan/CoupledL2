b8de5426030daa6d5278a1179d410d01eeb4c45a
lixin <1037997956@qq.com>
2023-04-18 10:22:03 +0800

    grantbufferfifo: syc grantBuffer commit ac3ede9

--- src/main/scala/coupledL2/GrantBufferFIFO.scala
+++ src/main/scala/coupledL2/GrantBufferFIFO.scala
@@ -122,7 +122,9 @@ class GrantBufferFIFO(implicit p: Parameters) extends BaseGrantBuffer with HasCi
     inflight_grant(bufIdx).valid := false.B
   }
 
-  // handle capacity conflict
+  // handle capacity conflict of GrantBuffer
+  // count the number of valid blocks + those in pipe that might use GrantBuf
+  // so that GrantBuffer will not exceed capacity
   val noSpaceForSinkReq = PopCount(Cat(VecInit(io.pipeStatusVec.tail.map { case s =>
     s.valid && (s.bits.fromA || s.bits.fromC)
   }).asUInt, block_valids)) >= mshrsAll.U
