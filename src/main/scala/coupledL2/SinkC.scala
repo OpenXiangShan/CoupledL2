75f8bac1ab80bff026285aa68b65a1b8c2905bae
CYYYC13 <75767324+CYYYC13@users.noreply.github.com>
2023-07-05 12:35:31 +0800

    isHit basic function from L3 to L2 in coupledL2 (#26)
    * RefillUnit: add isHit basic function
    indicates the refillunit block granted from L3 is hit in L3 or not
    
    * Add chiselDB (#24)
    
    * debug: move assertions of mainpipe to Monitor
    
    * Util: add chiselDB of mainpipe s3 info
    
    * Util: generate chisel_db.cpp/h to build
    
    * bump Utility: chiselDB related
    
    * Makefile: add mkdir build, otherwise chisel_db.cpp nowhere to generate
    
    * Util: fix chiselDB problems
    
    * miscs: update github CI workflow
    
    * miscs: update github CI workflow II
    
    * Update Makefile, remove mkdir -p, mill will do it
    
    ---------
    
    Co-authored-by: Chen Xi <48302201+Ivyfeather@users.noreply.github.com>

--- src/main/scala/coupledL2/SinkC.scala
+++ src/main/scala/coupledL2/SinkC.scala
@@ -135,6 +135,7 @@ class SinkC(implicit p: Parameters) extends L2Module {
   io.resp.respInfo.param := io.c.bits.param
   io.resp.respInfo.last := last
   io.resp.respInfo.dirty := io.c.bits.opcode(0)
+  io.resp.respInfo.isHit := io.c.bits.opcode(0)
 
   io.releaseBufWrite.valid := io.c.valid && io.c.bits.opcode === ProbeAckData
   io.releaseBufWrite.beat_sel := UIntToOH(beat)
