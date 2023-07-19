ca7c3532860432800ba5cd46d42d99f222a7bf6f
XiChen <chenxi171@mails.ucas.ac.cn>
2023-07-19 17:41:31 +0800

    MainPipe: if dirHit, should not read dir again

--- src/main/scala/coupledL2/MainPipe.scala
+++ src/main/scala/coupledL2/MainPipe.scala
@@ -562,7 +562,7 @@ class MainPipe(implicit p: Parameters) extends L2Module {
   when(req_s3.fromA) {
     alloc_state.s_refill := false.B
     alloc_state.w_grantack := req_prefetch_s3 || req_get_s3 || req_put_s3
-    alloc_state.w_replResp := cache_alias // need replRead when NOT cache_alias
+    alloc_state.w_replResp := dirResult_s3.hit // need replRead when NOT dirHit
     // need Acquire downwards
     when(need_acquire_s3_a || req_put_s3) {
       alloc_state.s_acquire := false.B
