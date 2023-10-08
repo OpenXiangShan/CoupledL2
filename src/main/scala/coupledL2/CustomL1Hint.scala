b29d6c7f46e32912cf911f29531859ae787b5036
XiChen <chenxi171@mails.ucas.ac.cn>
2023-10-08 14:40:47 +0800

    Hint: consider new feature ** AMergeTask **

--- src/main/scala/coupledL2/CustomL1Hint.scala
+++ src/main/scala/coupledL2/CustomL1Hint.scala
@@ -24,6 +24,11 @@ import org.chipsalliance.cde.config.Parameters
 import freechips.rocketchip.tilelink.TLMessages._
 import coupledL2.utils._
 
+class HintQueueEntry(implicit p: Parameters) extends L2Bundle {
+  val source = UInt(sourceIdBits.W)
+  val opcode = UInt(3.W)
+}
+
 class CustomL1HintIOBundle(implicit p: Parameters) extends L2Bundle {
   // input information
   val s1 = Flipped(ValidIO(new TaskBundle()))
@@ -36,11 +41,6 @@ class CustomL1HintIOBundle(implicit p: Parameters) extends L2Bundle {
   val l1Hint = DecoupledIO(new L2ToL1Hint())
 }
 
-class HintQueueEntry(implicit p: Parameters) extends L2Bundle {
-  val source = UInt(sourceIdBits.W)
-  val opcode = UInt(3.W)
-}
-
 // grantData hint interface
 // use this interface to give a hint to l1 before actually sending a GrantData
 class CustomL1Hint(implicit p: Parameters) extends L2Module {
@@ -50,22 +50,24 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
   val task_s3 = io.s3.task
   val mshrReq_s1 = task_s1.bits.mshrTask
   val mshrReq_s3 = task_s3.bits.mshrTask
+  val mergeA_s1  = task_s1.bits.mergeA
   val need_mshr_s3 = io.s3.need_mshr
 
   def isGrantData(t: TaskBundle):  Bool = t.fromA && t.opcode === GrantData
   def isGrant(t: TaskBundle):      Bool = t.fromA && t.opcode === Grant
-  def isHintAck(t: TaskBundle):    Bool = t.fromA && t.opcode === HintAck // HintAck has no effect on Hint, WARNINGTODO: merge req!!
-  def isReleaseAck(t: TaskBundle): Bool = t.fromC && t.opcode === ReleaseAck
+  def isHintAck(t: TaskBundle):    Bool = t.fromA && t.opcode === HintAck // HintAck has no effect on Hint
   def isRelease(t: TaskBundle):    Bool = t.fromC && (t.opcode === Release || t.opcode === ReleaseData)
+  def isMergeGrantData(t: TaskBundle): Bool = t.fromA && t.mergeA && t.aMergeTask.opcode === GrantData
+  def isMergeGrant(t: TaskBundle):     Bool = t.fromA && t.mergeA && t.aMergeTask.opcode === Grant
 
   // ==================== Hint Generation ====================
   // Hint for "MSHRTask and ReleaseAck" will fire@s1
-  val mshr_GrantData_s1 = task_s1.valid &&  mshrReq_s1 && isGrantData(task_s1.bits)
-  val mshr_Grant_s1     = task_s1.valid &&  mshrReq_s1 && isGrant(task_s1.bits)
+  val mshr_GrantData_s1 = task_s1.valid &&  mshrReq_s1 && (isGrantData(task_s1.bits) || isMergeGrantData(task_s1.bits))
+  val mshr_Grant_s1     = task_s1.valid &&  mshrReq_s1 && (isGrant(task_s1.bits) || isMergeGrant(task_s1.bits))
   val chn_Release_s1    = task_s1.valid && !mshrReq_s1 && isRelease(task_s1.bits)
 
   val enqValid_s1 = mshr_GrantData_s1 || mshr_Grant_s1 || chn_Release_s1
-  val enqSource_s1 = task_s1.bits.sourceId
+  val enqSource_s1 = Mux(task_s1.bits.mergeA, task_s1.bits.aMergeTask.sourceId, task_s1.bits.sourceId)
   val enqOpcode_s1 = ParallelPriorityMux(
     Seq(
       mshr_Grant_s1 -> Grant,
