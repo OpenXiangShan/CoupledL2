e8aae51cc17dadd609e7c2f3f3aead4a2da32636
Yanqin Li <liyanqin@bosc.ac.cn>
2023-12-26 03:34:00 +0800

    bop: add offset 117,147,91 on pcp

--- src/main/scala/coupledL2/prefetch/Prefetcher.scala
+++ src/main/scala/coupledL2/prefetch/Prefetcher.scala
@@ -265,7 +265,24 @@ class Prefetcher(implicit p: Parameters) extends PrefetchModule {
       })))
       val vbop = Module(new VBestOffsetPrefetch()(p.alterPartial({
         case L2ParamKey => p(L2ParamKey).copy(prefetch = Some(BOPParameters(
-          badScore = 2
+          badScore = 2,
+          offsetList = Seq(
+            -117,-147,-91,117,147,91,
+            -256, -250, -243, -240, -225, -216, -200,
+            -192, -180, -162, -160, -150, -144, -135, -128,
+            -125, -120, -108, -100, -96, -90, -81, -80,
+            -75, -72, -64, -60, -54, -50, -48, -45,
+            -40, -36, -32, -30, -27, -25, -24, -20,
+            -18, -16, -15, -12, -10, -9, -8, -6,
+            -5, -4, -3, -2, -1,
+            1, 2, 3, 4, 5, 6, 8,
+            9, 10, 12, 15, 16, 18, 20, 24,
+            25, 27, 30, 32, 36, 40, 45, 48,
+            50, 54, 60, 64, 72, 75, 80, 81,
+            90, 96, 100, 108, 120, 125, 128, 135,
+            144, 150, 160, 162, 180, 192, 200, 216,
+            225, 240, 243, 250/*, 256*/
+          )
         )))
       })))
       val tp = Module(new TemporalPrefetch()(p.alterPartial({
