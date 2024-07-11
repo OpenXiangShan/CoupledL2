6b009dd4770cb5f97d9b8140cbf9077f3cb47ab2
Tang Haojin <tanghaojin@outlook.com>
2024-07-11 17:13:53 +0800

    bump utility (OpenXiangShan/OpenLLC#3)

--- llc-src/src/test/scala/TestTop.scala
+++ llc-src/src/test/scala/TestTop.scala
@@ -94,7 +94,7 @@ class TestTopSoC(numCores: Int = 1, numULAgents: Int = 0, banks: Int = 1)(implic
   }
 
   // val l2 = LazyModule(new TL2CHICoupledL2())
-  val l2_nodes = (0 until numCores).map(i => LazyModule(new TL2CHICoupledL2()(new Config((_, _, _) => {
+  val l2_nodes = (0 until numCores).map(i => LazyModule(new TL2CHICoupledL2()(new Config((site, here, up) => {
     case L2ParamKey => cacheParams.copy(
       name                = s"L2_$i",
       hartId              = i,
@@ -102,6 +102,16 @@ class TestTopSoC(numCores: Int = 1, numULAgents: Int = 0, banks: Int = 1)(implic
     case EnableCHI => true
     case huancun.BankBitsKey => log2Ceil(banks)
     case MaxHartIdBits => log2Up(numCores)
+    case LogUtilsOptionsKey => LogUtilsOptions(
+      false,
+      here(L2ParamKey).enablePerf,
+      here(L2ParamKey).FPGAPlatform
+    )
+    case PerfCounterOptionsKey => PerfCounterOptions(
+      here(L2ParamKey).enablePerf && !here(L2ParamKey).FPGAPlatform,
+      here(L2ParamKey).enableRollingDB && !here(L2ParamKey).FPGAPlatform,
+      i
+    )
   }))))
 
   val l3 = LazyModule(new DummyLLC(numCores)(p))
