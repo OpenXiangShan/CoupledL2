5073c2a7b88fd4cb747b5947b36147365d6bee9c
Ding Haonan <kumonda@kucro3.org>
2024-10-29 11:08:58 +0800

    chore(TestTop): support for upwards CMO for CHI TestTop (#267)

--- src/test/scala/chi/TestTop.scala
+++ src/test/scala/chi/TestTop.scala
@@ -62,6 +62,9 @@ class TestTop_CHIL2(numCores: Int = 1, numULAgents: Int = 0, banks: Int = 1)(imp
     }
   }
 
+  val l1cmo_req_nodes = (0 until numCores).map(i => BundleBridgeSource(Some(() => DecoupledIO(new CMOReq))))
+  val l1cmo_resp_nodes = (0 until numCores).map(i => BundleBridgeSink(Some(() => DecoupledIO(new CMOResp))))
+
   // val l2 = LazyModule(new TL2CHICoupledL2())
   val l2_nodes = (0 until numCores).map(i => LazyModule(new TL2CHICoupledL2()(new Config((site, here, up) => {
     case L2ParamKey => cacheParams.copy(
@@ -99,6 +102,9 @@ class TestTop_CHIL2(numCores: Int = 1, numULAgents: Int = 0, banks: Int = 1)(imp
         TLLogger(s"L2_L1[${i}].UL[${j}]", !cacheParams.FPGAPlatform && cacheParams.enableTLLog) :=
         l1i
     }
+
+    l2.cmo_sink_node.foreach(_ := l1cmo_req_nodes(i))
+    l2.cmo_source_node.foreach(l1cmo_resp_nodes(i) := _)
     
     l2.managerNode :=
       TLXbar() :=*
@@ -145,6 +151,15 @@ class TestTop_CHIL2(numCores: Int = 1, numULAgents: Int = 0, banks: Int = 1)(imp
     mmio_nodes.zipWithIndex.foreach {
       case (node, i) =>
         node.makeIOs()(ValName(s"mmio_port_$i"))
+    }
+
+    if (cacheParams.hasCMO) {
+      l1cmo_req_nodes.zipWithIndex.foreach { case (node, i) =>
+        node.makeIOs()(ValName(s"cmo_req_$i"))  
+      }
+      l1cmo_resp_nodes.zipWithIndex.foreach { case (node, i) =>
+        node.makeIOs()(ValName(s"cmo_resp_$i"))  
+      }
     }
 
     val io = IO(Vec(numCores, new Bundle() {
@@ -195,6 +210,9 @@ object TestTopCHIHelper {
         enableCHILog        = !onFPGAPlatform && enableChiselDB && enableCHILog,
         elaboratedTopDown   = false,
         FPGAPlatform        = onFPGAPlatform,
+
+        // CMO
+        hasCMO              = true,
 
         // prefetch
         prefetch            = Seq(BOPParameters()),
