74977229e1e3c4954b1b67b22824ef05c3d6d8f1
Yanqin Li <maxpicca@qq.com>
2024-10-31 15:22:18 +0800

    fix(common): fix error width of vaddr

--- src/main/scala/coupledL2/Common.scala
+++ src/main/scala/coupledL2/Common.scala
@@ -373,7 +373,7 @@ class TlbExceptionBundle extends Bundle {
   val instr = Output(Bool())
 }
 class L2TlbReq(implicit p: Parameters) extends L2Bundle{
-  val vaddr = Output(UInt((fullVAddrBits+offsetBits).W))
+  val vaddr = Output(UInt((fullVAddrBits).W))
   val cmd = Output(TlbCmd())
   val isPrefetch = Output(Bool())
   val size = Output(UInt(log2Ceil(log2Ceil(XLEN/8) + 1).W))
