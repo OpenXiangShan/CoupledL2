7927cdd3b152566d5efa03a8e71b55f802a3c911
Ding Haonan <kumonda@kucro3.org>
2024-12-04 17:15:54 +0800

    feat(DataCheck): support dummy DataCheck and Poison (#19)
    * feat(DataCheck): support dummy DataCheck and Poison
    
    * Enabled DataCheck of CoupledL2 in TestTop.
    
    * submodule(CoupledL2): bump CoupledL2

--- Common.scala
+++ Common.scala
@@ -134,6 +134,8 @@ class TaskWithData(implicit p: Parameters) extends LLCBundle {
     dat.be := Fill(BE_WIDTH, true.B)
     dat.data := data.data(beatId).data
     dat.dataID := (beatBytes * beatId * 8).U(log2Ceil(blockBytes * 8) - 1, log2Ceil(blockBytes * 8) - 2)
+    dat.dataCheck := Cat((0 until DATACHECK_WIDTH).map(i => data.data(beatId).data(8 * (i + 1) - 1, 8 * i).xorR.asUInt))
+    dat.poision := 0.U
     dat
   }
 }
