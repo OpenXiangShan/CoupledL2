3fe904288022ae2facb6cde00eb6f0040f58a3f5
sumailyyc <yuanyuchong18@mails.ucas.ac.cn>
2024-12-16 16:10:03 +0800

    feat(OpenLLC): add support for ReadNoSnp transaction (#22)

--- RequestArb.scala
+++ RequestArb.scala
@@ -67,8 +67,9 @@ class RequestArb(implicit p: Parameters) extends LLCModule with HasClientInfo wi
   val isCleanInvalid_s1 = !task_s1.bits.refillTask && task_s1.bits.chiOpcode === CleanInvalid
   val isCleanShared_s1 = !task_s1.bits.refillTask && task_s1.bits.chiOpcode === CleanShared
   val isWriteCleanFull_s1 = !task_s1.bits.refillTask && task_s1.bits.chiOpcode === WriteCleanFull
+  val isReadNoSnp_s1 = !task_s1.bits.refillTask && task_s1.bits.chiOpcode === ReadNoSnp
 
-  val isRead_s1  = isReadNotSharedDirty_s1 || isReadUnique_s1
+  val isRead_s1  = isReadNotSharedDirty_s1 || isReadUnique_s1 || isReadNoSnp_s1
   val isClean_s1 = isCleanInvalid_s1 || isCleanShared_s1 || isWriteCleanFull_s1
 
   // To prevent data hazards caused by read-after-write conflicts in the directory,
