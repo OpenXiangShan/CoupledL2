f1d3ce09a0f9fa28630696fba045ec18ea4e1cf4
Gs-ygc <gs2ygc@gmail.com>
2025-01-07 11:36:43 +0800

    feat(matrix): add l2 matrix debug info

--- src/main/scala/coupledL2/SinkA.scala
+++ src/main/scala/coupledL2/SinkA.scala
@@ -24,8 +24,8 @@ import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tilelink.TLMessages._
 import freechips.rocketchip.tilelink.TLHints._
 import coupledL2.prefetch.PrefetchReq
-import huancun.{AliasKey, PrefetchKey}
-import utility.{MemReqSource, XSPerfAccumulate}
+import huancun.{AliasKey, PrefetchKey,PutBufferPop,PutBufferBeatEntry}
+import utility.{MemReqSource, XSPerfAccumulate, RRArbiterInit}
 
 class SinkA(implicit p: Parameters) extends L2Module {
   val io = IO(new Bundle() {
@@ -91,6 +91,94 @@ class SinkA(implicit p: Parameters) extends L2Module {
     task.cmoAll := cmoAllValid
     task
   }
+  when (io.a.fire && isPutFullData) {
+    when (hasData) {
+      when (first) {
+        //TODO Support Mask
+        dataBuf(nextPtr)(beat) := io.a.bits.data
+        beatValids(nextPtr)(beat) := true.B
+        printf(p"Put First message received: address=${Hexadecimal(io.a.bits.address)}, data=${Hexadecimal(io.a.bits.data)}\n")
+      }.otherwise {
+        assert(last)
+        dataBuf(nextPtrReg)(beat) := io.a.bits.data
+        beatValids(nextPtrReg)(beat) := true.B
+        printf(p"Put Last message received: address=${Hexadecimal(io.a.bits.address)}, data=${Hexadecimal(io.a.bits.data)}\n")
+      }
+    }
+  }
+
+  when (io.a.fire && isPutFullData && last) {
+    when (hasData) {
+      taskValids(nextPtrReg) := true.B
+      taskBuf(nextPtrReg) := fromTLAtoTaskBundle(io.a.bits)
+      taskBuf(nextPtrReg).bufIdx := nextPtrReg
+    }.otherwise {
+      taskValids(nextPtr) := true.B
+      taskBuf(nextPtr) := fromTLAtoTaskBundle(io.a.bits)
+      taskBuf(nextPtr).bufIdx := nextPtr
+    }
+  }
+
+  taskArb.io.out.ready := io.task.ready
+  taskArb.io.in.zipWithIndex.foreach {
+    case (in, i) =>
+      in.valid := taskValids(i)
+      in.bits := taskBuf(i)
+      when (in.fire) {
+        taskValids(i) := false.B
+      }
+  }
+
+  val aValid = io.a.valid && isPutFullData && last
+  io.task.valid := taskArb.io.out.valid
+  io.task.bits := taskArb.io.out.bits
+  io.task.bits.bufIdx := taskArb.io.out.bits.bufIdx
+
+  // io.resp.valid := io.a.valid && (first || last) && !isPutFullData
+  // io.resp.mshrId := 0.U // DontCare
+  // io.resp.tag := parseAddress(io.a.bits.address)._1
+  // io.resp.set := parseAddress(io.a.bits.address)._2
+  // io.resp.respInfo := 0.U.asTypeOf(io.resp.respInfo.cloneType)
+  // io.resp.respInfo.opcode := io.a.bits.opcode
+  // io.resp.respInfo.param := io.a.bits.param
+  // io.resp.respInfo.last := last
+  // io.resp.respInfo.dirty := io.a.bits.opcode(0)
+  // io.resp.respInfo.isHit := io.a.bits.opcode(0)
+  // io.resp.respInfo.corrupt := io.a.bits.corrupt
+
+  // keep the first beat of ProbeAckData
+  // val probeAckDataBuf = RegEnable(io.a.bits.data, 0.U((beatBytes * 8).W),
+  //   io.a.valid && io.a.bits.opcode === ProbeAckData && first)
+
+  // io.releaseBufWrite.valid := io.a.valid && io.a.bits.opcode === ProbeAckData && last
+  // io.releaseBufWrite.bits.id := 0.U(mshrBits.W) // id is given by MSHRCtl by comparing address to the MSHRs
+  // io.releaseBufWrite.bits.data.data := Cat(io.a.bits.data, probeAckDataBuf)
+  // io.releaseBufWrite.bits.beatMask := Fill(beatSize, true.B)
+
+  // // C-Release, with new data, comes before repl-Release writes old refill data back to DS
+  // val newdataMask = VecInit(io.msInfo.map(s =>
+  //   s.valid && s.bits.set === io.task.bits.set && s.bits.reqTag === io.task.bits.tag && s.bits.blockRefill
+  // )).asUInt
+
+  // we must wait until 2nd beat written into databuf(idx) before we can read it
+  // So we use RegNext
+  // //Or we can use Cat(databuf(idx)(0), io.a.bits.data)
+
+  // since what we are trying to prevent is that C-Release comes first and MSHR-Release comes later
+  // we can make sure this refillBufWrite can be read by MSHR-Release
+  // TODO: this is rarely triggered, consider just blocking? but blocking may affect timing of SinkC-Directory
+  // io.refillBufWrite.valid := RegNext(io.task.fire && io.task.bits.opcode === ReleaseData && newdataMask.orR, false.B)
+  // io.refillBufWrite.bits.id := RegNext(OHToUInt(newdataMask))
+  // io.refillBufWrite.bits.data.data := dataBuf(RegNext(io.task.bits.bufIdx)).asUInt
+  // io.refillBufWrite.bits.beatMask := Fill(beatSize, true.B)
+
+  io.a.ready := !isPutFullData || !first || !full
+
+  // io.bufResp.data := RegNext(RegEnable(dataBuf(io.task.bits.bufIdx), io.task.fire))
+  when(RegNext(io.task.fire)) {
+    beatValids(RegNext(io.task.bits.bufIdx)).foreach(_ := false.B)
+  }
+
   def fromPrefetchReqtoTaskBundle(req: PrefetchReq): TaskBundle = {
     val task = Wire(new TaskBundle)
     val fullAddr = Cat(req.tag, req.set, 0.U(offsetBits.W))
