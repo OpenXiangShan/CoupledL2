e820d7925ae7ee411a53aea173dfa35c20c71c41
zhanglinjuan <zhanglinjuan16@mails.ucas.ac.cn>
2025-01-10 14:51:58 +0800

    timing(Slice, BOP): avoid timing path from SRAM to ICG (#321)
    This commit removes critical timing paths from SRAM to ICG, mainly
    including:
    
    - DataStorage SRAM read result -> error -> regs in BusErrorUnit
    - RecentRequestTable SRAM result -> rrTable hit or not -> update
      ScoreTable
    
    Co-authored-by: Yanqin Li <maxpicca@qq.com>

--- src/main/scala/coupledL2/tl2chi/Slice.scala
+++ src/main/scala/coupledL2/tl2chi/Slice.scala
@@ -196,8 +196,8 @@ class Slice()(implicit p: Parameters) extends BaseSlice[OuterBundle]
   sinkC.io.c <> inBuf.c(io.in.c)
   io.in.d <> inBuf.d(grantBuf.io.d)
   grantBuf.io.e <> inBuf.e(io.in.e)
-  io.error.valid := mainPipe.io.error.valid
-  io.error.bits := mainPipe.io.error.bits
+  io.error.valid := RegNext(mainPipe.io.error.valid, false.B)
+  io.error.bits := RegNext(mainPipe.io.error.bits)
 
   /* Connect downwards channels */
   io.out.tx.req <> txreq.io.out
