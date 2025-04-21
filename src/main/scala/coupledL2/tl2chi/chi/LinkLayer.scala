54d31e7ab85ab3ad23104e493cb2503cd1f8f460
Zhu Yu <yulightenyu@gmail.com>
2025-04-21 10:17:53 +0800

    fix(Linklayer): deactive tx linkactive when exit coherency
    By following steps, it can be ensured the process of exit coherency and top linklayer
    a. start exit coherency by syscoreq
    b. confirm exiting Coherency completion by ssycoack
    c. Deassert txsactivereq and comfirm by txsactiveack
    d. rxactivereq will be triggered by ICN
    e. return L-credit and assert rxactiveack

--- src/main/scala/coupledL2/tl2chi/chi/LinkLayer.scala
+++ src/main/scala/coupledL2/tl2chi/chi/LinkLayer.scala
@@ -316,15 +316,16 @@ class LinkMonitor(implicit p: Parameters) extends L2Module with HasCHIOpcodes {
   LCredit2Decoupled(io.out.rx.rsp, io.in.rx.rsp, LinkState(rxState), rxrspDeact, Some("rxrsp"))
   LCredit2Decoupled(io.out.rx.dat, io.in.rx.dat, LinkState(rxState), rxdatDeact, Some("rxdat"))
 
-  io.out.txsactive := true.B
+  //exit coherecy + deactive tx/rx when l2 flush done
+  val exitco = io.exitco.getOrElse(false.B)
+  io.out.syscoreq := !exitco
+
+  io.out.txsactive := !exitco
   io.out.tx.linkactivereq := RegNext(true.B, init = false.B)
   io.out.rx.linkactiveack := RegNext(
     next = RegNext(io.out.rx.linkactivereq) || !rxDeact,
     init = false.B
   )
-  //exit coherecy + deactive tx/rx when l2 flush done
-  val exitco = io.exitco.getOrElse(false.B)
-  io.out.syscoreq := !exitco
 
   val retryAckCnt = RegInit(0.U(64.W))
   val pCrdGrantCnt = RegInit(0.U(64.W))
