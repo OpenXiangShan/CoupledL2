ab1bdff9519780f410ec68a46552a69137313a86
Kumonda221 <kumonda@kucro3.org>
2025-07-09 23:05:42 +0800

    chore(Debug): annotations for debug signals

---
+++ src/main/scala/oceanus/debug/DebugElement.scala
@@ -0,0 +1,11 @@
+package oceanus.debug
+
+import scala.annotation.StaticAnnotation
+
+
+/*
+* Annotation for debug-purpose elements.
+* 
+* * NOTICE: Reserved for furture use, recommended to embed this in code. 
+*/
+final class DebugElement extends StaticAnnotation
---
+++ src/main/scala/oceanus/debug/DebugSignal.scala
@@ -0,0 +1,11 @@
+package oceanus.debug
+
+import scala.annotation.StaticAnnotation
+
+
+/*
+* Annotation for debug signals.
+* 
+* * NOTICE: Reserved for furture use, recommended to embed this in code. 
+*/
+final class DebugSignal extends StaticAnnotation
