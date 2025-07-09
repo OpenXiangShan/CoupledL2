035f33e13df1a3588e54513a9a9b37ff8525df9f
Kumonda221 <kumonda@kucro3.org>
2025-07-09 23:05:42 +0800

    chore(Debug): annotations for debug signals

---
+++ src/main/scala/oceanus/debug/DebugBundle.scala
@@ -0,0 +1,11 @@
+package oceanus.debug
+
+import chisel3._
+
+
+/*
+* Base class for debug signal bundles.
+* 
+* * NOTICE: Reserved for furture use, recommended to embed this in code.
+*/
+class DebugBundle extends Bundle
