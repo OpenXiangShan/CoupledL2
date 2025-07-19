46c57e2df540127756c5725f71a829a0108df120
Kumonda221 <kumonda@kucro3.org>
2025-07-19 19:20:25 +0800

    feat(L2Common): common bundle definitions

--- src/main/scala/oceanus/l2/L2Common.scala
+++ src/main/scala/oceanus/l2/L2Common.scala
@@ -9,6 +9,15 @@ class L2LocalDirectoryState extends Bundle {
     def isV: Bool = L2LocalDirectoryState.isV(bits)
     def isU: Bool = L2LocalDirectoryState.isU(bits)
     def isD: Bool = L2LocalDirectoryState.isD(bits)
+
+    def asIntermediateState: L2LocalIntermediateState = {
+        val state = Wire(new L2LocalIntermediateState)
+        state.bits := L2LocalIntermediateState.fromDirectoryState(bits)
+        state
+    }
+
+    def ===(that: L2LocalDirectoryState): Bool = bits === that.bits
+    def =/=(that: L2LocalDirectoryState): Bool = bits =/= that.bits
 }
 
 object L2LocalDirectoryState {
@@ -27,6 +36,26 @@ object L2LocalDirectoryState {
     def isV(state: UInt): Bool = (state & BIT_VALID).orR
     def isU(state: UInt): Bool = (state & BIT_UNIQUE).orR
     def isD(state: UInt): Bool = (state & BIT_DIRTY).orR
+
+    def apply(): L2LocalDirectoryState = new L2LocalDirectoryState
+}
+
+class L2LocalIntermediateState extends Bundle {
+    val bits = UInt(L2LocalIntermediateState.WIDTH.W)
+
+    def isV: Bool = L2LocalIntermediateState.isV(bits)
+    def isU: Bool = L2LocalIntermediateState.isU(bits)
+    def isD: Bool = L2LocalIntermediateState.isD(bits)
+    def isPD: Bool = L2LocalIntermediateState.isPD(bits)
+
+    def asDirectoryState: L2LocalDirectoryState = {
+        val state = Wire(new L2LocalDirectoryState)
+        state.bits := L2LocalIntermediateState.asDirectoryState(bits)
+        state
+    }
+
+    def ===(that: L2LocalIntermediateState): Bool = bits === that.bits
+    def =/=(that: L2LocalIntermediateState): Bool = bits =/= that.bits
 }
 
 object L2LocalIntermediateState {
@@ -52,12 +81,36 @@ object L2LocalIntermediateState {
     def isPD(state: UInt): Bool = (state & BIT_PASSDIRTY).orR
 
     def asDirectoryState(state: UInt): UInt = state(L2LocalDirectoryState.WIDTH - 1, 0)
+    def fromDirectoryState(state: UInt): UInt = {
+        require(state.getWidth == L2LocalDirectoryState.WIDTH)
+        Cat(0.U((WIDTH - L2LocalDirectoryState.WIDTH).W), state)
+    }
+
+    def apply(): L2LocalIntermediateState = new L2LocalIntermediateState
+}
+
+class L2ClientState extends Bundle {
+    val bits = UInt(L2ClientState.WIDTH.W)
+
+    def isV: Bool = L2ClientState.isV(bits)
+    def isU: Bool = L2ClientState.isU(bits)
+
+    def ===(that: L2ClientState): Bool = bits === that.bits
+    def =/=(that: L2ClientState): Bool = bits =/= that.bits
 }
 
 object L2ClientState {
     val WIDTH = 2
 
-    def I = "b00".U(WIDTH.W) // Invalid
-    def S = "b01".U(WIDTH.W) // Shared
-    def U = "b11".U(WIDTH.W) // Unique
+    def BIT_VALID   = "b01".U(WIDTH.W) // Valid bit
+    def BIT_UNIQUE  = "b10".U(WIDTH.W) // Unique bit
+
+    def I = (0.U(WIDTH.W)          ) // Invalid
+    def S = (BIT_VALID             ) // Shared
+    def U = (BIT_VALID | BIT_UNIQUE) // Unique
+
+    def isV(state: UInt): Bool = (state & BIT_VALID).orR
+    def isU(state: UInt): Bool = (state & BIT_UNIQUE).orR
+
+    def apply(): L2ClientState = new L2ClientState
 }
