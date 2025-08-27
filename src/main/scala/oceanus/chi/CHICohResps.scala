29a8b076e2bf8dc2c0adf261144570503f384e9b
Kumonda221 <kumonda@kucro3.org>
2025-08-27 11:43:55 +0800

    feat(CHI): CHI coherent state responses

--- src/main/scala/oceanus/chi/CHICohResps.scala
+++ src/main/scala/oceanus/chi/CHICohResps.scala
@@ -1,3 +1,19 @@
 package oceanus.chi
 
+import chisel3._
+import chisel3.util._
 
+object CHICohResps {
+
+    val WIDTH = 3
+
+    def I       = "b000".U(WIDTH.W) // Invalid
+    def SC      = "b001".U(WIDTH.W) // Shared Clean
+    def UC      = "b010".U(WIDTH.W) // Unique Clean
+    def UD      = "b010".U(WIDTH.W) // Unique Dirty
+    def SD      = "b011".U(WIDTH.W) // Shared Dirty
+    def I_PD    = "b100".U(WIDTH.W) // Invalid, Pass Dirty
+    def SC_PD   = "b101".U(WIDTH.W) // Shared Clean, Pass Dirty
+    def UC_PD   = "b110".U(WIDTH.W) // Unique Clean, Pass Dirty
+    def SD_PD   = "b111".U(WIDTH.W) // Shared Dirty, Pass Dirty
+}
