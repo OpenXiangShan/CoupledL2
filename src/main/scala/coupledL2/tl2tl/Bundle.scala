a415123820958818a8a528a6be87e85528096743
Yanqin Li <maxpicca@qq.com>
2025-12-16 21:26:05 +0800

    refactor: refine prefetch statistics in topdown monitor (#452)
    * chore(pf): add miss_count for l2topdownmonitor
    
    * refactor(pf): vectorize the selection of prefetchers
    
    * chore(pf): more detailed prefetch statistics
    
    * refactor: replace pfHit by hitPf to show verb and object attributes
    
    * submodule(utility): bump for new buskey
    
    * chore: update the utility in yml into master for synchronization
    
    * fix(slice): give default value due to different option of prefetch
    
    * Revert "chore: update the utility in yml into master for synchronization"
    
    This reverts commit 75ef1811c2622fb897a4a04955b8196bb446ea74.
    
    merge refactor: refine prefetch statistics in topdown monitor

--- src/main/scala/coupledL2/tl2tl/Bundle.scala
+++ src/main/scala/coupledL2/tl2tl/Bundle.scala
@@ -16,38 +16,3 @@
   */
 
 package coupledL2.tl2tl
-
-import chisel3._
-import chisel3.util._
-import org.chipsalliance.cde.config.Parameters
-import freechips.rocketchip.tilelink.TLPermissions._
-import utility.MemReqSource
-import coupledL2._
-
-// MSHR exposes signals to MSHRCtl
-class MSHRStatus(implicit p: Parameters) extends L2Bundle with HasTLChannelBits {
-  val set         = UInt(setBits.W)
-  val reqTag      = UInt(tagBits.W)
-  val metaTag     = UInt(tagBits.W)
-  val needsRepl = Bool()
-  val w_c_resp = Bool()
-  val w_d_resp = Bool()
-  val will_free = Bool()
-
-  //  val way = UInt(wayBits.W)
-//  val off = UInt(offsetBits.W)
-//  val opcode = UInt(3.W)
-//  val param = UInt(3.W)
-//  val size = UInt(msgSizeBits.W)
-//  val source = UInt(sourceIdBits.W)
-//  val alias = aliasBitsOpt.map(_ => UInt(aliasBitsOpt.get.W))
-//  val aliasTask = aliasBitsOpt.map(_ => Bool())
-//  val needProbeAckData = Bool() // only for B reqs
-//  val fromL2pft = prefetchOpt.map(_ => Bool())
-//  val needHint = prefetchOpt.map(_ => Bool())
-
-  // for TopDown usage
-  val reqSource = UInt(MemReqSource.reqSourceBits.W)
-  val is_miss = Bool()
-  val is_prefetch = Bool()
-}
