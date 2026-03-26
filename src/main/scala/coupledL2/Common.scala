ef78c095ed3c50f8b5520fb411d734b227bdd679
Frankslu <1653954675@qq.com>
2026-03-26 16:18:54 +0800

    fix(Common, CoupledL2): redundant width in l2Hint sourceId

--- src/main/scala/coupledL2/Common.scala
+++ src/main/scala/coupledL2/Common.scala
@@ -399,8 +399,8 @@ class PrefetchRecv extends Bundle {
 }
 
 // custom l2 - l1 interface
-class L2ToL1Hint(implicit p: Parameters) extends Bundle {
-  val sourceId = UInt(32.W)    // tilelink sourceID
+class L2ToL1Hint(implicit p: Parameters) extends L2Bundle {
+  val sourceId = UInt(sourceIdBits.W)    // tilelink sourceID
   val isKeyword = Bool()       // miss entry keyword
 }
 
