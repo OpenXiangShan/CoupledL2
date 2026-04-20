3f24716b9c5e6b9ec1b9385719274ed91a31f881
Kumonda221 <kumonda@kucro3.org>
2026-04-20 18:39:54 +0800

    feat(TestTop): support for performance counters

--- src/test/scala/oceanus/TestTop.scala
+++ src/test/scala/oceanus/TestTop.scala
@@ -12,6 +12,11 @@ import oceanus.chi._
 
 class TestTop_L2TSHRAlloc()(implicit val p: Parameters) extends Module with HasL2Params {
 
+  val log = IO(new Bundle {
+    val dump = Input(Bool())
+    val clean = Input(Bool())
+  })
+
   val io = IO(new Bundle {
 
     val fromTSHR = Flipped(Vec(paramL2.mshrSize, Valid(new L2TSHRAlloc.PathFromTSHR)))
@@ -22,10 +27,28 @@ class TestTop_L2TSHRAlloc()(implicit val p: Parameters) extends Module with HasL
 
   val config = new L2TSHRAllocConfig(
     cluster = Seq(Seq(L2TSHRAllocTarget.EVT), Seq(L2TSHRAllocTarget.SNP, L2TSHRAllocTarget.REQ)),
-    resv = Seq((15, L2TSHRResvTarget.L2EVT), (14, L2TSHRResvTarget.L1EVT), (13, L2TSHRResvTarget.L3SNP))
+    resv = Seq(
+      (paramL2.mshrSize - 1, L2TSHRResvTarget.L2EVT),
+      (paramL2.mshrSize - 2, L2TSHRResvTarget.L1EVT),
+      (paramL2.mshrSize - 3, L2TSHRResvTarget.L3SNP))
   )
 
   println(s"${config.cluster}")
+
+  val timer = WireDefault(0.U(64.W))
+  val logEnable = WireDefault(false.B)
+  val clean = WireDefault(false.B)
+  val dump = WireDefault(false.B)
+
+  timer := 0.U
+  logEnable := true.B
+  clean := log.clean
+  dump := log.dump
+
+  dontTouch(timer)
+  dontTouch(logEnable)
+  dontTouch(clean)
+  dontTouch(dump)
 
   val module = Module(new L2TSHRAlloc(config))
 
@@ -33,6 +56,9 @@ class TestTop_L2TSHRAlloc()(implicit val p: Parameters) extends Module with HasL
   module.io.fromTSHRCtrl <> io.fromTSHRCtrl
 
   io.toTSHR := module.io.toTSHR
+
+  //
+  XSLog.collect(timer, logEnable, clean, dump)
 }
 
 object TestTopFirtoolOptions {
@@ -50,13 +76,24 @@ object TestTop_L2TSHRAlloc extends App {
   val config = new Config((_, _, _) => {
     case L2ParamsKey => L2Params (
       physicalAddrWidth = 48,
-      mshrSize = 16
+      mshrSize = 8
     )
     case CHIParametersKey => CHIParameters (
       issue = EnumCHIIssue.E,
       nodeIdWidth = 11,
       reqAddrWidth = 48,
       dataWidth = 256
+    )
+    case LogUtilsOptionsKey => LogUtilsOptions(
+      enableDebug = false,
+      enablePerf = true,
+      fpgaPlatform = false
+    )
+    case PerfCounterOptionsKey => PerfCounterOptions (
+      enablePerfPrint = true,
+      enablePerfDB = false,
+      perfLevel = XSPerfLevel.VERBOSE,
+      0
     )
   })
 
