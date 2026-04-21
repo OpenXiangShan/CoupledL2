510f682e049c3bb6eeb1e0f57b6eb1261a39c018
Frankslu <1653954675@qq.com>
2026-04-22 01:32:56 +0800

    fix: better arb

--- src/main/scala/coupledL2/utils/Arb.scala
+++ src/main/scala/coupledL2/utils/Arb.scala
@@ -51,7 +51,6 @@ abstract class L2FastArbiterBase[T <: Data](val gen: T, val n: Int) extends Modu
 }
 
 class L2FastArbiter[T <: Data](gen: T, n: Int) extends L2FastArbiterBase[T](gen, n) {
-
   val chosenOH = Wire(UInt(n.W))
   val valids = VecInit(io.in.map(_.valid)).asUInt
   // the reqs that we didn't choose in last cycle
@@ -85,80 +84,86 @@ class L2FastArbiter[T <: Data](gen: T, n: Int) extends L2FastArbiterBase[T](gen,
 }
 
 
-// TwoLevelRRArbiter is similar to mux of two RRArbiters, reducing the input width of MaskToOH
 class TwoLevelRRArbiter[T <: Data](gen: T, n: Int) extends L2FastArbiterBase[T](gen, n) {
-  if (n >= 2){
-    val mid = n / 2
-    val rest = n - mid
-
-    val chosenOHLow = Wire(UInt(mid.W))
-    val chosenOHHigh = Wire(UInt(rest.W))
-    val valids = VecInit(io.in.map(_.valid)).asUInt
-    val validsLow = valids(mid - 1, 0)
-    val validsHigh = valids(n - 1, mid)
-    val fireLow, fireHigh = WireDefault(false.B)
-
-    val selLow = RegInit(false.B)
-    val selNext = WireInit(!selLow)
-    when (validsLow.orR && validsHigh.orR) {
-      selLow := !selLow
-    }.elsewhen (validsLow.orR) {
-      selLow := true.B
-      selNext := true.B
-    }.elsewhen (validsHigh.orR) {
-      selLow := false.B
-      selNext := false.B
-    }
-
-    val pendingMaskLow = RegEnable(
-      validsLow & (~chosenOHLow).asUInt,
-      0.U(mid.W),
-      fireLow
-    )
-    val pendingMaskHigh = RegEnable(
-      validsHigh & (~chosenOHHigh).asUInt,
-      0.U(rest.W),
-      fireHigh
-    )
-    val rrGrantMaskLow = RegEnable(VecInit((0 until mid) map { i =>
-      if(i == 0) false.B else chosenOHLow(i - 1, 0).orR
-    }).asUInt, 0.U(mid.W), fireLow)
-    val rrGrantMaskHigh = RegEnable(VecInit((0 until rest) map { i =>
-      if(i == 0) false.B else chosenOHHigh(i - 1, 0).orR
-    }).asUInt, 0.U(rest.W), fireHigh)
-    val rrSelOHLow = VecInit(maskToOH((rrGrantMaskLow & pendingMaskLow).asBools)).asUInt & Cat(Seq.fill(mid)(selLow))
-    val rrSelOHHigh = VecInit(maskToOH((rrGrantMaskHigh & pendingMaskHigh).asBools)).asUInt & Cat(Seq.fill(rest)(!selLow))
-    val rrSelOH = Cat(rrSelOHHigh, rrSelOHLow)
-    assert(PopCount(rrSelOH) <= 1.U)
-    val rrValid = (rrSelOH & valids).orR
-    val firstOneOH = VecInit(maskToOH(valids.asBools)).asUInt
-    val chosenOH = Mux(rrValid, rrSelOH, firstOneOH)
-    chosenOHLow := chosenOH(mid - 1, 0)
-    chosenOHHigh := chosenOH(n - 1, mid)
-    assert(PopCount(chosenOH) <= 1.U)
-    fireLow := (chosenOHLow & validsLow).orR
-    fireHigh := (chosenOHHigh & validsHigh).orR
-
-    io.out.valid := valids.orR
-    io.out.bits := Mux1H(chosenOH, io.in.map(_.bits))
-
-    io.in.map(_.ready).zip(chosenOH.asBools).foreach{
-      case (rdy, grant) => rdy := grant && io.out.ready
-    }
-
-    io.chosen := OHToUInt(chosenOH)
-    io.chosenOH := chosenOH
-  } else if (n == 1) {
+  if (n == 1) {
+    // for better codegen
     io.out.valid := io.in.head.valid
     io.out.bits := io.in.head.bits
     io.in.head.ready := io.out.ready
     io.chosen := 0.U
     io.chosenOH := 1.U
-  } else {
+  } else if (n == 0) {
     io.out.valid := false.B
-    io.out.bits := gen
-    io.in.foreach(_.ready := false.B)
+    io.out.bits := 0.U.asTypeOf(io.out.bits)
     io.chosen := 0.U
     io.chosenOH := 0.U
+  } else {
+    val mid = n / 2
+    val rest = n - mid
+    val lowOut = Wire(Decoupled(gen))
+    val highOut = Wire(Decoupled(gen))
+    val selLow = RegInit(false.B)
+    val finalSelLow = Wire(Bool())
+
+    val chosenOHLow = Wire(UInt(mid.W))
+    val chosenOHHigh = Wire(UInt(rest.W))
+    val validsLow = VecInit(io.in.take(mid).map(_.valid)).asUInt
+    val validsHigh = VecInit(io.in.drop(mid).map(_.valid)).asUInt
+
+    val pendingMaskLow = RegEnable(
+      validsLow & (~chosenOHLow).asUInt, // make IDEA happy ...
+      0.U(mid.W),
+      lowOut.fire
+    )
+    val pendingMaskHigh = RegEnable(
+      validsHigh & (~chosenOHHigh).asUInt, // make IDEA happy ...
+      0.U(rest.W),
+      highOut.fire
+    )
+
+    val rrGrantMaskLow = RegEnable(VecInit((0 until mid) map { i =>
+      if(i == 0) false.B else chosenOHLow(i - 1, 0).orR
+    }).asUInt, 0.U(mid.W), lowOut.fire)
+    val rrGrantMaskHigh = RegEnable(VecInit((0 until rest) map { i =>
+      if(i == 0) false.B else chosenOHHigh(i - 1, 0).orR
+    }).asUInt, 0.U(rest.W), highOut.fire)
+
+    val rrSelOHLow = VecInit(maskToOH((rrGrantMaskLow & pendingMaskLow).asBools)).asUInt
+    val rrSelOHHigh = VecInit(maskToOH((rrGrantMaskHigh & pendingMaskHigh).asBools)).asUInt
+    val firstOneOHLow = VecInit(maskToOH(validsLow.asBools)).asUInt
+    val firstOneOHHigh = VecInit(maskToOH(validsHigh.asBools)).asUInt
+    val rrValidLow = (rrSelOHLow & validsLow).orR
+    val rrValidHigh = (rrSelOHHigh & validsHigh).orR
+    chosenOHLow := Mux(rrValidLow, rrSelOHLow, firstOneOHLow)
+    chosenOHHigh := Mux(rrValidHigh, rrSelOHHigh, firstOneOHHigh)
+
+    lowOut.valid := validsLow.orR
+    lowOut.bits := Mux1H(chosenOHLow, io.in.take(mid).map(_.bits))
+    highOut.valid := validsHigh.orR
+    highOut.bits := Mux1H(chosenOHHigh, io.in.drop(mid).map(_.bits))
+
+    io.in.take(mid).map(_.ready).zip(chosenOHLow.asBools).foreach{
+      case (rdy, grant) => rdy := grant && lowOut.ready
+    }
+    io.in.drop(mid).map(_.ready).zip(chosenOHHigh.asBools).foreach{
+      case (rdy, grant) => rdy := grant && highOut.ready
+    }
+
+    io.out.valid := lowOut.valid || highOut.valid
+    finalSelLow := OHMux(Seq(
+      (lowOut.valid && highOut.valid) -> selLow,
+      (lowOut.valid && !highOut.valid) -> true.B,
+      (!lowOut.valid && highOut.valid) -> false.B
+    ))
+    io.out.bits := Mux(finalSelLow, lowOut.bits, highOut.bits)
+    lowOut.ready := io.out.ready && finalSelLow
+    highOut.ready := io.out.ready && !finalSelLow
+
+    io.chosenOH := Cat(Seq.fill(rest)(!finalSelLow).asUInt & chosenOHHigh, Seq.fill(mid)(finalSelLow).asUInt & chosenOHLow)
+    io.chosen := OHToUInt(io.chosenOH)
+
+    when (io.out.fire) {
+      selLow := Mux(finalSelLow, !highOut.valid, lowOut.valid)
+    }
   }
 }
