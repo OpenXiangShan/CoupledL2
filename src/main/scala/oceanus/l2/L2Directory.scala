ca728491e0dea3622f2d2209e648d6b7eaf9348f
Kumonda221 <kumonda@kucro3.org>
2026-05-14 15:42:57 +0800

    feat(L2Directory): draft of persistent Meta state

--- src/main/scala/oceanus/l2/L2Directory.scala
+++ src/main/scala/oceanus/l2/L2Directory.scala
@@ -9,6 +9,45 @@ import org.chipsalliance.cde.config.Parameters
 
 object L2Directory {
 
+  object MetaState {
+
+    def apply() = UInt(2.W)
+
+    def UU: UInt = 3.U(2.W)
+    def US: UInt = 2.U(2.W)
+    def S: UInt = 1.U(2.W)
+    def I: UInt = 0.U(2.W)
+
+    def isClientU(state: UInt, client: Bool) = {
+      Mux(state === UU, client, false.B)
+    }
+
+    def isClientS(state: UInt, client: Bool) = {
+      Mux(state === US || state === S, client, false.B)
+    }
+
+    def toLocal(state: UInt): L2CacheLocalState = {
+      val value = Wire(new L2CacheLocalState)
+      value.bits := ParallelLookUp(state, Seq(
+        (UU, L2CacheStates.U),
+        (US, L2CacheStates.U),
+        (S , L2CacheStates.S),
+        (I , L2CacheStates.I)
+      ))
+      value
+    }
+  }
+
+  class Meta(implicit val p: Parameters) extends Bundle with HasL2Params {
+    val way = UInt(4.W) // TODO: parameterize with l2 way count
+    val state = MetaState()
+    val dirty = Bool()
+    val clients = UInt(1.W) // TODO: parameterize with coherent l2 client count
+  }
+
+  class Tag extends Bundle {
+
+  }
   class PathToDirectory(implicit val p: Parameters) extends Bundle with HasL2Params {
 
     val TSHRADDR = UInt(mshrIndexWidth.W)
@@ -23,7 +62,7 @@ object L2Directory {
   class PathFromDirectory(implicit val p: Parameters) extends Bundle with HasL2Params {
 
     val TSHRADDR = UInt(mshrIndexWidth.W)
-    val META = Bool() // TODO: replace with meta bundle
+    val META = new L2Directory.Meta // TODO: replace with meta bundle
 
     val DirRdArbComp = Bool()
     val DirRdResp = Bool()
