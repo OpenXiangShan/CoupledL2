81ee1439517030bc808d801a73e81c35d9039cec
Kumonda221 <kumonda@kucro3.org>
2026-06-12 20:17:19 +0800

    feat(L2Common): performance counter utilities

--- src/main/scala/oceanus/l2/L2Common.scala
+++ src/main/scala/oceanus/l2/L2Common.scala
@@ -4,51 +4,46 @@ import chisel3._
 import chisel3.util._
 import oceanus.compactchi.CCHIComponent
 import org.chipsalliance.cde.config.Parameters
+import oceanus.compactchi.CCHIOpcode
+import oceanus.compactchi.CCHIChannelType
+import utility.ParallelOR
+import utility.ParallelMux
+import oceanus.chi.EnumCHIChannel.SNP
+import utility.XSPerfAccumulate
+import utility.XSPerfHistogram
 
 object L2Address {
-    def set(pa: UInt)(implicit p: Parameters): UInt = pa // TODO: extract real set index from PA
+  def set(pa: UInt)(implicit p: Parameters): UInt = pa // TODO: extract real set index from PA
 }
 
-class L2DSReadMask extends Bundle {
-    val S = Bool()
-    val U = Bool()
-
-    def test[T <: L2CacheStateTrait](state: T): Bool = {
-        S && state.isS || U && state.isU
+object FSMPerfHistogram {
+  def apply(name: String, prev: Bool, next: Bool)(implicit p: Parameters): Unit = {
+    val counter = RegInit(0.U(32.W))
+    when (prev) {
+      when (!next) {
+        counter := 0.U
+      }.otherwise {
+        counter := counter + 1.U
+      }
     }
-
-    def testDirMeta(state: UInt): Bool = {
-        test(L2Directory.MetaState.toLocal(state))
-    }
+    XSPerfAccumulate(name + "_total", prev)
+    XSPerfHistogram(name, counter, prev && !next, 0, 40, 2, right_strict = true)
+    XSPerfHistogram(name, counter, prev && !next, 40, 800, 40, left_strict = true)
+  }
 }
 
-object L2DSReadMask {
-
-    def never = {
-        val value = Wire(new L2DSReadMask)
-        value.S := false.B
-        value.U := false.B
-        value
+object FSMTransPerfHistogram {
+  def apply(name: String, prev: Bool, next: Bool)(implicit p: Parameters): Unit = {
+    val counter = RegInit(0.U(32.W))
+    when (prev) {
+      when (next) {
+        counter := 0.U
+      }.otherwise {
+        counter := counter + 1.U
+      }
     }
-
-    def whenS = {
-        val value = Wire(new L2DSReadMask)
-        value.S := true.B
-        value.U := false.B
-        value
-    }
-
-    def whenU = {
-        val value = Wire(new L2DSReadMask)
-        value.S := false.B
-        value.U := true.B
-        value
-    }
-
-    def always = {
-        val value = Wire(new L2DSReadMask)
-        value.S := true.B
-        value.U := true.B
-        value
-    }
+    XSPerfAccumulate(name + "_cnt", prev && next)
+    XSPerfHistogram(name, counter, prev && next, 0, 40, 2, right_strict = true)
+    XSPerfHistogram(name, counter, prev && next, 40, 800, 40, left_strict = true)
+  }
 }
