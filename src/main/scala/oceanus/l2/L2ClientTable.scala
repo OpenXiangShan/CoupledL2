7ec2563e5f0955a59f7970649f7b0b34b4daae4a
Kumonda221 <kumonda@kucro3.org>
2026-08-15 19:00:00 +0800

    feat(L2ClientTable): turned to bank top connections

--- src/main/scala/oceanus/l2/L2ClientTable.scala
+++ src/main/scala/oceanus/l2/L2ClientTable.scala
@@ -7,23 +7,25 @@ import oceanus.l2._
 import org.chipsalliance.cde.config.Parameters
 
 
-class L2ClientTable()(implicit val p: Parameters) extends Module with HasL2Params {
+class L2ClientTable(val bank: Int)(implicit val p: Parameters) extends Module with HasL2Params {
 
-    val theOnlyDCacheNID = 0
+  val theOnlyDCacheNID = 0
 
-    val io = IO(new Bundle {
-        val query0 = Input(Vec(paramL2.mshrSize, UInt(8.W))) // TODO: configurable with upstream nodeId width
-        val clients0 = Output(Vec(paramL2.mshrSize, Vec(1, Bool()))) // TODO: parameterize with coherent l2 client count
+  val io = IO(new Bundle {
+    val queryREQ = Input(Vec(bank, Vec(paramL2.mshrSize, UInt(8.W)))) // TODO: configurable with upstream nodeId width
+    val clientsREQ = Output(Vec(bank, Vec(paramL2.mshrSize, Vec(1, Bool())))) // TODO: parameterize with coherent l2 client count
 
-        val query1 = Input(Vec(paramL2.mshrSize, UInt(8.W))) // TODO: configurable with upstream nodeId width
-        val clients1 = Output(Vec(paramL2.mshrSize, Vec(1, Bool()))) // TODO: parameterize with coherent l2 client count
-    })
+    val queryEVT = Input(Vec(bank, Vec(paramL2.mshrSize, UInt(8.W)))) // TODO: configurable with upstream nodeId width
+    val clientsEVT = Output(Vec(bank, Vec(paramL2.mshrSize, Vec(1, Bool())))) // TODO: parameterize with coherent l2 client count
+  })
 
-    io.query0.zip(io.clients0).foreach { case (query, client) => {
-        client := query === theOnlyDCacheNID.U
-    }}
+  io.queryREQ.zip(io.clientsREQ).foreach { case (query, clients) => {
+    query.zip(clients).foreach { case (query, clients) =>
+      clients.head := query === theOnlyDCacheNID.U
+  }}}
 
-    io.query1.zip(io.clients1).foreach { case (query, client) => {
-        client := query === theOnlyDCacheNID.U
-    }}
+  io.queryEVT.zip(io.clientsEVT).foreach { case (query, clients) => {
+    query.zip(clients).foreach { case (query, clients) =>
+      clients.head := query === theOnlyDCacheNID.U
+  }}}
 }
